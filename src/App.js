1eccaa1fcf67fa1c967921673f65addc47ea25f6
Rehe-4i5 <kuronekoryohei@gmail.com>
2023-05-16 19:18:59 +0900

    [add]axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  get axios() {
+    const axiosBase = require('axios');
+    return axiosBase.create({
+        baseURL: "http://localhost:3001",
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Requested-With': 'XMLHttpRequest'
+        },
+        responseType: 'json'
+    });
+  }
+
+  
+
+
 }
 
 export default App;
