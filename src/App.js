f092a1ef24a6216043ea52603d880a1ab8f6c5b4
Rehe-4i5 <kuronekoryohei@gmail.com>
2023-05-16 21:47:36 +0900

    [complete]rails-apiとの通信

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Post from "./components/Post";
 
 class App extends React.Component {
-  get axios() {
-    const axiosBase = require('axios');
-    return axiosBase.create({
-        baseURL: "http://localhost:3001",
-        headers: {
-            'Content-Type': 'application/json',
-            'X-Requested-With': 'XMLHttpRequest'
-        },
-        responseType: 'json'
-    });
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            createFormInputs: {
+                title: "",
+                content: "",
+            },
+            posts: [],
+        }
+    }
 
-  
+    get axios() {
+        const axiosBase = require('axios');
+        return axiosBase.create({
+            baseURL: process.env.REACT_APP_DEV_API_URL,
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest'
+            },
+            responseType: 'json'
+        });
+    }
 
+    componentDidMount() {
+        this.axios.get('/posts')
+            .then(results => {
+                console.log(results);
+                this.setState({
+                    posts: results.data
+                });
+            })
+            .catch(data => {
+                console.log(data);
+            });
+    }
 
+    getPosts() {
+        return (
+            this.state.posts.map((post) => {
+                return (
+                  <Post post={post} />
+                )
+            })
+        );
+    }
+
+    render() {
+        return (
+            <div className="App">
+                            {this.getPosts()}
+            </div>
+        );
+    }
 }
 
 export default App;
