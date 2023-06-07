92eccd25a0e245f5d538e0b154db9a47a8f20ed0
Rehe-4i5 <kuronekoryohei@gmail.com>
2023-06-07 18:29:56 +0900

    [fix]layout

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,38 +5,34 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 
 class Post extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            modalOpen: false,
-            editFormOpen: false,
-            editFormInputs: {
-                title: "",
-                body: "",
-            },
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalOpen: false,
+      editFormOpen: false,
+      editFormInputs: {
+        title: "",
+        body: "",
+      },
+    };
+  }
 
-    render() {
-        return (
-            <div>
-                <Card>
-                    <CardContent>
-                        <Typography variant="h3" component="h3"
-                        >
-                            {this.props.post.title}
-                        </Typography>
-                        <Typography variant="body2" 
-                        >
-                            {this.props.post.body}
-                        </Typography>
-
-                    </CardContent>
-                </Card>
-
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Card
+          variant="outlined"
+        >
+          <CardContent>
+            <Typography variant="h3" component="h3">
+              {this.props.post.title}
+            </Typography>
+            <Typography variant="body2">{this.props.post.body}</Typography>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
 }
 
 export default Post;
